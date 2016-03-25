44cb791ddc861539fe42813560eb00de0afd636d
LosNinos <losninos@byu506110wks.app.byu.edu>
2016-03-25 09:56:04 -0600

    starting working on the editing funtionality delete is working rename is buggy

--- MapsTutorial/StartPage.swift
+++ MapsTutorial/StartPage.swift
@@ -62,4 +62,77 @@ class StartPage: UIViewController, UITableViewDataSource, UITableViewDelegate {
         }
     }
     
+    
+   // set up for delete and edit tabs on swipe
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+// I think this is for if we want to change the styling of the swipe
+    func tableView(tableView: (UITableView!), commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: (NSIndexPath!)) {
+        
+    }
+// handles the actions of buttons in the swipe
+    func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
+        
+        var deleteAction = UITableViewRowAction(style: .Default, title: "Delete") {action in
+            //handle delete
+            print("Delete \(self.trips![indexPath.row].getName())")
+            print("row index \(indexPath.row)")
+            print(self.trips!.count)
+            
+            //remove the trip from the model
+            self.trips?.removeAtIndex(indexPath.row)
+        
+            //remove the trip from the view
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            print(self.trips!.count)
+            
+            //save edited trips array to file
+            let isSuccessfulSave = NSKeyedArchiver.archiveRootObject( self.trips!, toFile: FileSaveStaticData.ArchiveURL.path!)
+            if !isSuccessfulSave {
+                print("Failed to save trips...")
+            }
+            else {
+                print("Successful save!")
+            }
+        }
+        
+        
+        var inputTextField: UITextField?
+        let passwordPrompt = UIAlertController(title: "Rename Trip", message: " ", preferredStyle: UIAlertControllerStyle.Alert)
+        passwordPrompt.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+        passwordPrompt.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            // Now do whatever you want with inputTextField (remember to unwrap the optional)
+            print("input String: "+inputTextField!.text!)
+            self.trips![indexPath.row].setTripName(inputTextField!.text!)
+            
+            let cell = tableView.cellForRowAtIndexPath(indexPath)
+            cell?.textLabel!.text = inputTextField!.text!
+            
+            
+            //save edited trips array to file
+            let isSuccessfulSave = NSKeyedArchiver.archiveRootObject( self.trips!, toFile: FileSaveStaticData.ArchiveURL.path!)
+            if !isSuccessfulSave {
+                print("Failed to save trips...")
+            }
+            else {
+                print("Successful save!")
+            }
+            
+        }))
+        passwordPrompt.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+            textField.placeholder = "New Trip Name"
+            inputTextField = textField
+        })
+        
+       
+        
+        var editAction = UITableViewRowAction(style: .Normal, title: "Edit") {action in
+           print("Edit \(self.trips![indexPath.row].getName())")
+             self.presentViewController(passwordPrompt, animated: true, completion: nil)
+        }
+        
+        return [deleteAction, editAction]
+    }
+    
 }
